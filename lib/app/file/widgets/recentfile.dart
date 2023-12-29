f944eee61fc3360feced55d1d7cb67413f85b983
Sagor <sagorsamadder51@gmail.com>
2023-12-29 19:26:28 +0600

    recent file viewing and limit only five file as recent

--- lib/app/file/widgets/recentfile.dart
+++ lib/app/file/widgets/recentfile.dart
@@ -5,6 +5,8 @@ import 'package:sdrive/general/utils/color.dart';
 import 'package:sdrive/general/utils/fonts_style.dart';
 import 'package:velocity_x/velocity_x.dart';
 
+import '../../view_file/view/view_file_screen.dart';
+
 class RecentFile extends StatelessWidget {
   const RecentFile({super.key});
 
@@ -24,57 +26,66 @@ class RecentFile extends StatelessWidget {
           return SizedBox(
             height: 150,
             child: ListView.builder(
-                itemCount: controller.recentfilesList.length,
+                itemCount: controller.recentfilesList.length > 5
+                    ? 5
+                    : controller.recentfilesList.length,
                 scrollDirection: Axis.horizontal,
                 itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.only(right: 10),
-                    child: Container(
-                      height: 150,
-                      width: 110,
-                      // padding: const EdgeInsets.all(5),
-                      decoration: BoxDecoration(
-                          border:
-                              Border.all(color: Colors.grey.withOpacity(0.5)),
-                          borderRadius: BorderRadius.circular(10)),
-                      child: Column(
-                        children: [
-                          controller.recentfilesList[index].fileType == "image"
-                              ? ClipRRect(
-                                  borderRadius: const BorderRadius.only(
-                                    topLeft: Radius.circular(10.0),
-                                    topRight: Radius.circular(10.0),
-                                  ),
-                                  child: Image.network(
-                                    controller.recentfilesList[index].url,
-                                    width: double.infinity,
-                                    height: 100,
-                                    fit: BoxFit.cover,
-                                  ),
-                                )
-                              : ClipRRect(
-                                  child: Padding(
-                                    padding: const EdgeInsets.only(top: 8),
-                                    child: Image(
+                  return InkWell(
+                    onTap: () {
+                      Get.to(() => ViewFileScreen(
+                          file: controller.recentfilesList[index]));
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 10),
+                      child: Container(
+                        height: 150,
+                        width: 110,
+                        // padding: const EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                            border:
+                                Border.all(color: Colors.grey.withOpacity(0.5)),
+                            borderRadius: BorderRadius.circular(10)),
+                        child: Column(
+                          children: [
+                            controller.recentfilesList[index].fileType ==
+                                    "image"
+                                ? ClipRRect(
+                                    borderRadius: const BorderRadius.only(
+                                      topLeft: Radius.circular(10.0),
+                                      topRight: Radius.circular(10.0),
+                                    ),
+                                    child: Image.network(
+                                      controller.recentfilesList[index].url,
                                       width: double.infinity,
                                       height: 100,
                                       fit: BoxFit.cover,
-                                      image: AssetImage(
-                                          'assets/icons/${controller.recentfilesList[index].fileExtension}.png'),
+                                    ),
+                                  )
+                                : ClipRRect(
+                                    child: Padding(
+                                      padding: const EdgeInsets.only(top: 8),
+                                      child: Image(
+                                        width: double.infinity,
+                                        height: 100,
+                                        fit: BoxFit.cover,
+                                        image: AssetImage(
+                                            'assets/icons/${controller.recentfilesList[index].fileExtension}.png'),
+                                      ),
                                     ),
                                   ),
-                                ),
-                          10.heightBox,
-                          Padding(
-                            padding: const EdgeInsets.only(left: 4),
-                            child: Text(
-                              controller.recentfilesList[index].name,
-                              style:
-                                  textStyle(13, Colors.black, FontWeight.w300),
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                          )
-                        ],
+                            10.heightBox,
+                            Padding(
+                              padding: const EdgeInsets.only(left: 4),
+                              child: Text(
+                                controller.recentfilesList[index].name,
+                                style: textStyle(
+                                    13, Colors.black, FontWeight.w300),
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            )
+                          ],
+                        ),
                       ),
                     ),
                   );
