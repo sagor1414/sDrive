1f644d58fb2d94acbd42951dd7cd49ad8b1bd4a4
Sagor <sagorsamadder51@gmail.com>
2023-12-30 11:52:20 +0600

    storage controller

---
+++ lib/app/home/controller/storage_controller.dart
@@ -0,0 +1,32 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
+import 'package:sdrive/general/utils/utils.dart';
+
+class StorageController extends GetxController {
+  String uid = FirebaseAuth.instance.currentUser!.uid;
+  RxInt size = 0.obs;
+  @override
+  void onInit() {
+    super.onInit();
+    getStorage();
+  }
+
+  getStorage() async {
+    size.bindStream(userCollection
+        .doc(uid)
+        .collection('files')
+        .snapshots()
+        .map((QuerySnapshot query) {
+      int size = 0;
+      query.docs.forEach((element) {
+        size = size + extractsize(element);
+      });
+      return size;
+    }));
+  }
+
+  int extractsize(QueryDocumentSnapshot<Object?> element) {
+    return element['size'];
+  }
+}
